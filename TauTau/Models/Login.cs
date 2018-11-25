3aef9e7e3d09607c5bf0099b4d4fd128df20a571
ShinLabo <hiroshinlabo@gmail.com>
2018-11-25 16:24:56 +0900

    create_database Login

---
+++ TauTau/Models/Login.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace TauTau.Models
+{
+    public class Login
+    {
+        [DisplayName("ログインID")]
+        public string Id { get; set; }
+
+        [DisplayName("パスワード")]
+        public string Password { get; set; }
+
+        [DisplayName("パスワード確認")]
+        public string Password_Second { get; set; }
+
+        [DisplayName("メールアドレス")]
+        public string EMail { get; set; }
+    }
+}
